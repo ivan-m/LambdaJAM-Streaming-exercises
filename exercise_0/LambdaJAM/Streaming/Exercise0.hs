e3c2e96375d72deb0860c3dcc769e2a5638ecb6c
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2018-05-16 16:38:19 +1000

    Create introductory exercise

---
+++ exercise_0/LambdaJAM/Streaming/Exercise0.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE DeriveFunctor #-}
+{- |
+   Module      : LambdaJAM.Streaming.Exercise0
+   Description : Exercise 0
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   In this exercise, you will build up a simplistic version of the
+   Stream data type and functions for it.
+
+ -}
+module LambdaJAM.Streaming.Exercise0 where
+
+--------------------------------------------------------------------------------
+
+{-
+
+As it currently stands, the 'Stream' data type here is isomorphic to
+@([a],r)@.
+
+  * @a@ is the elements of the 'Stream'
+
+  * @r@ is the final result from running the 'Stream'.
+
+-}
+data Stream a r = Step a (Stream a r)
+                | Return r
+  deriving (Functor)
+
+--------------------------------------------------------------------------------
+
+{-
+
+Task 1:
+
+Implement 'streamToList' and 'listToStream'.  These will serve as our
+initial methods of creating/running 'Stream's.
+
+-}
+
+streamToList :: Stream a r -> [a]
+streamToList = error "streamToList"
+
+listToStream :: [a] -> Stream a ()
+listToStream = error "listToStream"
+
+--------------------------------------------------------------------------------
+
+{-
+
+Task 2:
+
+As this current 'Stream' implementation is isomorphic to a list, it
+doesn't actually serve any purpose.  The true power of the 'Stream'
+type comes about when we allow a /monadic effect/.
+
+Augment the 'Stream' datatype to add in the ability to have a monadic
+action returning another 'Stream'.
+
+The canonical type parameters for this are @Stream a m r@.
+
+Make sure to update 'streamToList' and 'listToStream'!
+
+Ideally you would also be able to define 'Applicative' and 'Monad'
+instances for 'Stream'.
+
+-}
+
+--------------------------------------------------------------------------------
+
+{-
+
+Task 3:
+
+We typically do not want to pattern match every time we interact with
+a 'Stream'; instead, it would be convenient to have what we expect
+from many of our functional data structures: a folding function:
+
+> foldStream :: (Monad m) => (x -> a -> x) -> x -> (x -> b) -> Stream a m r -> m (b, r)
+
+(The @(x -> b)@ parameter will often be the 'id' function; i.e. @x ~ b@.)
+
+You may wish to re-implement 'streamToList' to use this function (and
+possibly make 'listToStream' take a 'Foldable').
+
+Try using this function to implement Stream analogues of functions
+like 'map', 'length', 'mconcat', etc.
+
+-}
+
+--------------------------------------------------------------------------------
+
+{-
+
+Task 4:
+
+We still have that monadic parameter, so let's allow for it.  Try
+defining Stream analogues of 'mapM' and 'mapM_' (you may wish to
+define a monadic fold function as well).
+
+Use these to do the equivalent of:
+
+> mapM_ print . map (*2) $ [1..10]
+
+-}
---
+++ exercise_0/README.md
@@ -0,0 +1,20 @@
+Exercise 0
+==========
+
+In this exercise we will build a simple variant of the `Stream` data
+type and see how it can be used.
+
+Completing this exercise is optional, but may give you a better
+understanding of how the Stream data type work.
+
+Tasks
+-----
+
+1. Implement `streamToList` and `listToStream`.
+
+2. Augment the `Stream` datatype to add in the ability to have a
+   monadic action returning another `Stream`.
+
+3. Implement `foldStream`.
+
+4. Implement monadic mappings.
