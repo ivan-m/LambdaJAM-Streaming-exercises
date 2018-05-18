1c624bb03b8476a9257686916e1e6fee9baa0475
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2018-05-18 15:15:16 +1000

    Add shell.nix for 0 and 1

---
+++ exercise_1/shell.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, conduit, stdenv, transformers }:
+      mkDerivation {
+        pname = "exercise1";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base conduit transformers ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
